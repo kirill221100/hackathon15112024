5e03cfac7efef8808230e5b4a41b3deb4f1d6b33
kirill221100 <kirulkacool@gmail.com>
2024-11-16 19:41:02 +0700

    commit

--- routes/team.py
+++ routes/team.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Depends
 from db.db_setup import get_session
+from schemes.team import CreateTeamScheme
 from utils.team import compatibility_between_two_candidates_func
 from security.auth import get_current_user
 
 team_router = APIRouter()
+
+@team_router.post('/create_team')
+async def create_team_path(team_data: CreateTeamScheme, current_user=Depends(get_current_user), session=Depends(get_session)):
+    
 
 
 @team_router.get('/compatibility-between-two-candidates')
